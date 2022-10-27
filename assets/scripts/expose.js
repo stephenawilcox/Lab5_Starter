22eff2146c22e45c87187d2c4fa11c6ebcf5fc55
Stephen Wilcox <stephenawilcox@gmail.com>
2022-10-26 21:11:55 -0700

    worked on images

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,26 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  var hornSelect = document.getElementById('horn-select');
+  var volControl = document.getElementById('volume-controls');
+  var img = document.querySelectorAll('img');
+
+  hornSelect.addEventListener('change', (event) => {
+    var horn = hornSelect.value;
+    switch(horn) {
+      case "air-horn":
+        img[0].src = 'assets/images/air-horn.svg';
+        
+        break;
+      case "car-horn":
+        img[0].src = 'assets/images/car-horn.svg';
+        
+        break;
+      case "party-horn":
+        img[0].src = 'assets/images/party-horn.svg';
+
+        break;
+    }
+  });
+
 }
