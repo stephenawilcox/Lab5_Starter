1ae4da490bfe5eb1016a03fa2cf4be24ec3fa539
Stephen Wilcox <stephenawilcox@gmail.com>
2022-10-27 00:12:54 -0700

    worked on speechsynthesis

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,86 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+/*
+window.addEventListener('DOMContentLoaded', function(){
+  console.log("hello");
+  const synth = window.speechSynthesis;
+  var voiceSelect = document.getElementById('voice-select');
+  let voices = [];
+  voices = synth.getVoices();
+  
+    for (let i = 0; i < voices.length ; i++) {
+      console.log("hello");
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+  
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+});*/
+
 
 function init() {
   // TODO
+  const synth = window.speechSynthesis;
+
+  var textArea = document.getElementById('text-to-speak');
+  var voiceSelect = document.getElementById('voice-select');
+  var button = document.querySelector('button');
+  var img = document.querySelectorAll('img');
+
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length ; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+  
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+  window.onload = populateVoiceList();
+  
+//Loading voices on page load wasnt working correctly on my chrome browser but it worked on safari
+//The getVoices function was returning an empty array
+  voiceSelect.addEventListener('click', (event) => {
+    if (voiceSelect.length == 1){ 
+      populateVoiceList();
+    };
+  });
+  
+  button.addEventListener('click', (event) => {
+    img[0].src = 'assets/images/smiling-open.png';
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+      speechSynthesis.onvoiceschanged = populateVoiceList;
+    }
+    const utterThis = new SpeechSynthesisUtterance(textArea.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    
+    for (let i = 0; i < voices.length ; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    synth.speak(utterThis);
+  
+    textArea.blur();
+    
+    
+  });
+
+
+
 }
